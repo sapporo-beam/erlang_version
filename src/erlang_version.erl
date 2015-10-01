7efed3e77a0d09763cf6d9702af58291e76041fc
niku <niku@niku.name>
2015-10-01 20:56:13 +0900

    Implement minor_version/0

--- src/erlang_version.erl
+++ src/erlang_version.erl
@@ -6,7 +6,8 @@
 
 %% API exports
 -export([version_to_binary/0,
-         major_version/0]).
+         major_version/0,
+         minor_version/0]).
 
 %%====================================================================
 %% API functions
@@ -15,6 +16,10 @@ version_to_binary() -> "18.1".
 
 major_version() ->
     erlang:system_info(otp_release).
+
+minor_version() ->
+    [_, MinorPart] = string:tokens(erlang:system_info(version), "."),
+    MinorPart.
 
 %%====================================================================
 %% Internal functions
@@ -26,6 +31,10 @@ major_version() ->
 -ifdef(TEST).
 major_version_test() ->
     ?assertEqual("18", major_version()).
+
+minor_version_test() ->
+    ?assertEqual("1", minor_version()).
+
 version_to_binary_test() ->
     ?assertEqual("18.1", version_to_binary()).
 -endif.
