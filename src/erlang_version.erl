32c3ab978061f121e7d226c17c95f8be1b30fcdd
niku <niku@niku.name>
2015-10-01 20:53:51 +0900

    Implement major_version/0

--- src/erlang_version.erl
+++ src/erlang_version.erl
@@ -5,12 +5,16 @@
 -endif.
 
 %% API exports
--export([version_to_binary/0]).
+-export([version_to_binary/0,
+         major_version/0]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
 version_to_binary() -> "18.1".
+
+major_version() ->
+    erlang:system_info(otp_release).
 
 %%====================================================================
 %% Internal functions
@@ -20,6 +24,8 @@ version_to_binary() -> "18.1".
 %% Tests
 %%====================================================================
 -ifdef(TEST).
+major_version_test() ->
+    ?assertEqual("18", major_version()).
 version_to_binary_test() ->
     ?assertEqual("18.1", version_to_binary()).
 -endif.
